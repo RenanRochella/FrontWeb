f5d9f6a1a8b912b79fbfac47f4dd1f791a160cbe
RenanRochella <reveg48@gmail.com>
2023-07-27 21:47:41 -0300

    Lá ele

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 const titulo = document.getElementById("titulo");
+const nomeInput = document.getElementById("nome");
+const emailInput = document.getElementById("email");
+const foneInput = document.getElementById("fone");
+
+const resultadoNome = document.getElementById("result-nome")
+const resultadoEmail = document.getElementById("result-email")
+const resultadoFone = document.getElementById("result-fone")
 
 titulo.textContent = "Senai Cotia 2023";
+
+function save() { 
+    resultadoNome.textContent = nomeInput.value
+    resultadoEmail.textContent = emailInput.value
+    resultadoFone.textContent = foneInput.value
+}
